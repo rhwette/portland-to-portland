c5632e324d6607070615049c1cbce995803dee61
richard wetterauer <rhwette2022@gmail.com>
2023-05-04 16:15:44 -0500

    page structure set

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React from 'react';
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import { Grid, GridItem, Box } from '@chakra-ui/react';
+import './App.css';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+    <Box className="container">
+      <Box marginRight={'10px'} marginLeft={'10px'} h="100%">
+        <Grid
+          templateAreas={`  "header"
+                                  "intro"
+                                  "locations"
+                                  "visit"
+                                  "footer"
+                                  `}
+        >
+          <GridItem area="header">{/* <Header></Header> */}</GridItem>
+
+          <GridItem area="intro">{/* <Intro /> */}</GridItem>
+
+          <GridItem area="locations">{/* <Locations /> */}</GridItem>
+
+          <GridItem area="visit">{/* <Visit /> */}</GridItem>
+
+          <GridItem area="footer" bg="black">
+            {/* <Footer /> */}
+          </GridItem>
         </Grid>
       </Box>
-    </ChakraProvider>
+    </Box>
   );
 }
 
