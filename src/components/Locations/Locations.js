ee1f4c3b295273a10945dc003fab311dd96e7598
richard wetterauer <rhwette2022@gmail.com>
2023-05-06 15:57:17 -0500

    locations component working now

--- src/components/Locations/Locations.js
+++ src/components/Locations/Locations.js
@@ -15,6 +15,8 @@ const Locations = () => {
           <Text className="locationCity_name">Kyiv, Ukraine</Text>
           <Image className="locationCity_picture" src={gridImage1a} />
           <Button
+            fontSize={'12px'}
+            colorScheme="blue"
             className="locationCity_button"
             href="https://www.forbes.com/advisor/investing/nft-non-fungible-token/"
           >
@@ -29,22 +31,22 @@ const Locations = () => {
           </div>
 
           <Text className="locationCity_descript">
-            <Text className="locationCity_description">
+            <span className="locationCity_description">
               Kyiv (or Kiev), Ukraine's capital, is a large city located on the
               shores of the Dnipro river. Of course, no one in their right mind
               would swim in the river, unless they grew up here, in which case
               they've probably tried it at some point. Summers are hot here, and
               the winters are drafty, but the fall and the spring are are
               absolutely amazing.
-            </Text>
-            <Text className="locationCity_description">
+            </span>
+            <span className="locationCity_description">
               The city itself is a mix of pre-revolution, post-war and soviet
               architecture, all of it sprinkled with modified balconies. If
               you're on the right bank of the Dnipro, the landscape is difficult
               to traverse for people on bikes and those wearing heels. The left
               bank is considered far less interesting and prestigious though,
               even by people who live on the far outskirts of the right one.
-            </Text>
+            </span>
           </Text>
         </Box>
 
@@ -54,6 +56,8 @@ const Locations = () => {
           <Text className="locationCity_name">Cricieth, Wales</Text>
           <Image className="locationCity_picture" src={criciethImage} />
           <Button
+            fontSize={'12px'}
+            colorScheme="blue"
             className="locationCity_button"
             href="https://www.forbes.com/advisor/investing/nft-non-fungible-token/"
           >
@@ -71,14 +75,14 @@ const Locations = () => {
           </div>
 
           <Text className="locationCity_descript">
-            <Text className="locationCity_description">
+            <span className="locationCity_description">
               The medieval ruin of Cricieth Castle overlooks the town below from
               a rock that juts out into the sea. It is thought to have been
               built by Llewelyn the Great in the 13th century. Some 900 years
               later, the self-styled Pearl of Wales on the Shores of Snowdonia
               has become a popular tourist destination during the summer months.
-            </Text>
-            <Text className="locationCity_description">
+            </span>
+            <span className="locationCity_description">
               A short walk down the road from the castle, you can enjoy the
               world's best ice cream at Cadwalader's, whose secret ingredient is
               rumored to be locally sourced seaweed. Another claim to fame is
@@ -86,7 +90,7 @@ const Locations = () => {
               in a row for its spectacular floral displays around town. It was
               also home to David Lloyd George, the only Welshman ever to hold
               the office of Prime Minister of the UK.
-            </Text>
+            </span>
           </Text>
         </Box>
 
@@ -96,6 +100,8 @@ const Locations = () => {
           <Text className="locationCity_name">Berea, USA</Text>
           <Image className="locationCity_picture" src={bereaImage} />
           <Button
+            fontSize={'12px'}
+            colorScheme="blue"
             className="locationCity_button"
             href="https://www.forbes.com/advisor/investing/nft-non-fungible-token/"
           >
@@ -110,7 +116,7 @@ const Locations = () => {
           </div>
 
           <Text className="locationCity_descript">
-            <Text className="locationCity_description">
+            <span className="locationCity_description">
               Berea is a small town located in the central part of Kentucky. The
               town is surrounded by beautiful forests and countryside. It's
               known as the arts and crafts capital of the state, and visitors
@@ -118,14 +124,14 @@ const Locations = () => {
               jewelry, candles, wood items, galleries, glass studios, and more.
               The town holds a yearly festival celebrating "spoonbread", a local
               dish made with cornbread and served with a wooden spoon.
-            </Text>
-            <Text className="locationCity_description">
+            </span>
+            <span className="locationCity_description">
               However, it's probably known best for the local college. Berea
               College was founded in 1855 and was the first college in the south
               to be racially integrated, as well as the first to be
               coeducational. Somewhat uniquely, it charges no tuition — every
               student receives a full-tuition scholarship.
-            </Text>
+            </span>
           </Text>
         </Box>
 
@@ -135,6 +141,8 @@ const Locations = () => {
           <Text className="locationCity_name">Berea, USA</Text>
           <Image className="locationCity_picture" src={muramvyaImage} />
           <Button
+            fontSize={'12px'}
+            colorScheme="blue"
             className="locationCity_button"
             href="https://www.forbes.com/advisor/investing/nft-non-fungible-token/"
           >
@@ -149,29 +157,29 @@ const Locations = () => {
           </div>
 
           <Text className="locationCity_descript">
-            <Text className="locationCity_description">
+            <span className="locationCity_description">
               Muramvya is one of the 18 provinces of Burundi. In the kingdom
               era, Muramvya was the royal capital and in 2007, because of its
               cultural and natural landscape, it was added to the UNESCO World
               Heritage Tentative List. It is located in the center of Burundi,
               between the country's political and economic capitals
-            </Text>
-            <Text className="locationCity_description">
+            </span>
+            <span className="locationCity_description">
               The weather is rather chilly at night, but during the day, you
               would think you're in heaven. At 2,665 meters (8,743 ft) above sea
               level, Mount Teza is one of the coldest places in the province.
               But that cool breeze allows for one of the largest plantations of
               tea and coffee in the country, which make up the bulk of Burundi's
               exports.
-            </Text>
-            <Text className="locationCity_description">
+            </span>
+            <span className="locationCity_description">
               The Kibira National Park, one of the largest wildlife reserves for
               apes, overlaps four provinces, including Muramvya. This National
               Park lies at the apex of the beautiful mountains of the Congo-Nile
               Divide, ranging between 1550 and 2660 meters of altitude. It is
               filled with beautiful vegetation, and a source for the various
               rivers and streams which provide water around the country.
-            </Text>
+            </span>
           </Text>
         </Box>
 
@@ -181,6 +189,8 @@ const Locations = () => {
           <Text className="locationCity_name">Hometown, Earth</Text>
           <Image className="locationCity_picture" src={hometownImage} />
           <Button
+            fontSize={'12px'}
+            colorScheme="blue"
             className="locationCity_button"
             href="https://www.forbes.com/advisor/investing/nft-non-fungible-token/"
           >
@@ -193,12 +203,12 @@ const Locations = () => {
           </div>
 
           <Text className="locationCity_descript">
-            <Text className="locationCity_description">
+            <span className="locationCity_description">
               Hometown, Earth.... a fascinating location
-            </Text>
-            <Text className="locationCity_description">
+            </span>
+            <span className="locationCity_description">
               Each of us has a Hometown, Earth, that we call our own.
-            </Text>
+            </span>
           </Text>
         </Box>
       </Box>
